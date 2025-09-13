1a700411fde3595bbbb05397c9957e97975b6444
Giyermion2002 <guillermo.martinezgarcia2002@gmail.com>
2025-09-13 18:20:38 +0200

    Feature: Crear la página de inicio Fixes #11. Cambiado el tipo de fuente de la pagina.

--- oniriko-frontend/src/main.tsx
+++ oniriko-frontend/src/main.tsx
@@ -1,10 +1,19 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./i18n";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Arsenica Trial', sans-serif",
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 );
